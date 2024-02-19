37951e3452823b445c53529e38343b0975560d45
Adamyr159 <adamyr159@Gmail.com>
2024-02-19 15:31:50 +0700

    Upload praktikum 1

---
+++ praktikum1/index.php
@@ -0,0 +1,13 @@
+<?php 
+// Mendefinisikan Variable
+$nama = "Adam Yusron";
+$umur = 19;
+$berat = 60.5;
+
+echo $nama;
+echo "<br>Umur " . $nama . " adalah " . $umur . " tahun";
+echo "<br>Berat badan = " . $berat . " Kg";
+
+
+
+?>
