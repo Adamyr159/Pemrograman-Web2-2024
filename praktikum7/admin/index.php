86413ca9b87465fdcebb331139768bf2a5efcb5b
Adamyr159 <adamyr159@Gmail.com>
2024-05-02 19:14:35 +0700

    Update Authentication fitur

--- praktikum7/admin/index.php
+++ praktikum7/admin/index.php
@@ -3,7 +3,7 @@ require_once 'navbar.html';
 require_once 'sidebar.html';
 ?>
 
-<h2>Selamat Datang Admin</h2>
+<h2>Selamat Datang <?= $_SESSION['user']['username']; ?></h2>
 
 <?php
 require_once 'footer.html';
