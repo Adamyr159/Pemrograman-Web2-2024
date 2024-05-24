b814dfabddf4878f73463f4c58f5ec4c84237be6
Adamyr159 <adamyr159@Gmail.com>
2024-05-24 10:54:36 +0700

    Laravel 3 - Databse & Model

--- puskesmas-new/resources/views/admin/index.blade.php
+++ puskesmas-new/resources/views/admin/index.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
-    
+    <x-slot name="page_name">Halaman Beranda</x-slot>
+    <x-slot name="page_content">Selamat Datang di Halaman Admin</x-slot>
 </x-layout>
--- puskesmas-new/routes/web.php
+++ puskesmas-new/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\KelurahanController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,3 +25,7 @@ Route::get('/profile', function () {
 // Praktikum Laravel 2
 Route::get('/dashboard', [AdminController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
+
+
+// Praktikum Laravel 3
+Route::get('/dashboard/kelurahan', [KelurahanController::class, 'index']);
